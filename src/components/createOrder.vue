510480881858b8172267c4f33e2ada256e984108
wangchangkui <35455776@qq.com>
2019-04-10 23:00:40 +0800

    测试跳转公众号

--- src/components/createOrder.vue
+++ src/components/createOrder.vue
@@ -20,12 +20,12 @@
       <div class="txt">{{item.specname}} <span v-if="item.show"><span class="price">{{item.discount}}</span>元</span></div>
       <input v-if="item.show" type="button" class="btn" value="购买" @click="onConfirmOrder(index)" />
     </div>
-    <div><span class="iconfont icon-dizhi"></span></div>
+    <!--<div><input type="button" value="跳转公众号" @tap="onGoWx"></div>-->
   </div>
 </template>
 
 <script>
-/* global setTitle */
+/* global setTitle, WeixinJSBridge */
 export default {
   name: 'createOrder',
   data () {
@@ -183,6 +183,22 @@ export default {
           }
         })
       })
+    },
+    // 跳转公众号测试
+    onGoWx () {
+      if (typeof WeixinJSBridge === 'undefined') {
+        if (document.addEventListener) {
+          document.addEventListener('WeixinJSBridgeReady', this.onBridgeReady, false)
+        } else if (document.attachEvent) {
+          document.attachEvent('WeixinJSBridgeReady', this.onBridgeReady)
+          document.attachEvent('onWeixinJSBridgeReady', this.onBridgeReady)
+        }
+      } else {
+        this.onBridgeReady()
+      }
+    },
+    onBridgeReady () {
+      WeixinJSBridge.call('closeWindow')
     }
   }
 }
@@ -191,9 +207,9 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
 .container{height: 100%; overflow: hidden;}
-.bg{position: absolute; left: 0; top: 0; z-index: 2; width: 100%; height: 2rem; background: #FFEAA3;}
+.bg{position: absolute; left: 0; top: 0; z-index: 2; width: 100%; height: 1.2rem; background: #FFEAA3;}
 .addr_box{
-  display: flex; flex-direction: column; justify-content: space-between; position: relative; z-index: 3; margin: 1rem .2rem 0; padding: .4rem .2rem .2rem; width: 6.4rem; background: #fff; border-radius: 10px; box-shadow: 0 2px 2px #fff2c4; color: #333; text-align: left;
+  display: flex; flex-direction: column; justify-content: space-between; position: relative; z-index: 3; margin: .5rem .2rem 0; padding: .4rem .2rem .2rem; width: 6.4rem; background: #fff; border-radius: 10px; box-shadow: 0 2px 2px #fff2c4; color: #333; text-align: left;
   .title {
     display: flex; flex-direction: row; justify-content: flex-start; flex: 1; font-size: .4rem; font-weight: bold; color: #000;
     .icon{
